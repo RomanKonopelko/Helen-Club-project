d007d26af1d215922067825040d914625108735f
RomanKonopelko <romank761@gmail.com>
2021-07-28 17:27:29 +0300

    Updates

--- src/js/scrolling.js
+++
@@ -1,7 +0,0 @@
- $('a[href*="#"]').on('click', function (e) {
-     e.preventDefault();
-
-     $('html, body').animate({
-         scrollTop: $($(this).attr('href')).offset().top
-     }, 700, 'linear');
- });
